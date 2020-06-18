83dfff81f8b64c596916d68a21b47fd8bd3cfd1e
Antonin <antoninlefevre45@icloud.com>
2020-06-18 22:26:19 +0200

    Update analyse_virus.py

--- analyse_virus.py
+++ analyse_virus.py
@@ -4,15 +4,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 from sklearn.datasets import make_blobs
 import random as rd
-
-""" I) création du dataset """
-
-x,y = make_blobs(n_samples=100,centers=1,cluster_std=0.5)
-#plt.scatter(x[:,0],x[:,1],c='dodgerblue')
-#plt.show()
 
 def distance(x,y): #distance entre 2 points du plan cartésien
     x1 = x[0]
@@ -21,92 +14,6 @@ def distance(x,y): #distance entre 2 points du plan cartésien
     y2 = y[1]
     return(abs(np.sqrt((y1-x1)**2+(y2-x2)**2)))
 
-""" II) choix de la personne infectée en premier """
-
-taille_pop = len(x)
-
-numero_infecte_1 = rd.randint(0,taille_pop)
-coord_1er_infecte = [x[:,0][numero_infecte_1],x[:,1][numero_infecte_1]]
-
-# afficher l'individu infecté dans la pop :
-
-plt.scatter(x[:,0],x[:,1],c='b')
-plt.scatter(x[:,0][numero_infecte_1],x[:,1][numero_infecte_1],c='r')
-plt.scatter(x[:,0][numero_infecte_1],x[:,1][numero_infecte_1],c='r',s=3300,alpha=0.2)
-plt.title('1er individu contaminé')
-plt.show()
-
-""" III) 1er contaminations """
-
-coord_infectes_0 = [coord_1er_infecte]
-coord_sains_0 = []
-
-# contamination
-
-for k in range (taille_pop):
-    if distance(coord_1er_infecte,[x[:,0][k],x[:,1][k]]) < 0.5 and k != numero_infecte_1 :
-        plt.scatter(x[:,0][k],x[:,1][k],c='r')
-        coord_infectes_0.append([x[:,0][k],x[:,1][k]])
-    else :
-        plt.scatter(x[:, 0][k], x[:, 1][k], c='b')
-        coord_sains_0.append([x[:, 0][k], x[:, 1][k]])
-    plt.scatter(x[:, 0][numero_infecte_1], x[:, 1][numero_infecte_1], c='r')
-    plt.title('1er vague de contamination')
-    plt.show()
-
-plt.clf()
-plt.pie([len(coord_infectes_0),len(coord_sains_0)],labels=('infectés','sains'),shadow=True,autopct='%1.1f%%')
-plt.title('1er vague de contamination')
-plt.show()
-
-""" IV) 2e contamination """
-
-coord_sains_0 # les sains au début
-cas_sains_1 = [] # les individus sains sont de moins en moins
-coord_infectes_1 = coord_infectes_0 # on va rajouter les malades
-
-for k in range (len(coord_infectes_0)):
-    for j in range (len(coord_sains_0)):
-        if distance(np.array(coord_infectes_0)[k,:],np.array(coord_sains_0)[j,:]) <0.5 and np.array(coord_sains_0)[j,:] not in np.array(coord_infectes_1) :
-            coord_infectes_1.append(np.array(coord_sains_0)[j,:])
-        elif np.array(coord_sains_0)[j,:] not in np.array(cas_sains_1) :
-            cas_sains_1.append(np.array(coord_sains_0)[j,:])
-
-plt.scatter(np.array(cas_sains_1)[:,0],np.array(cas_sains_1)[:,1],c='b')
-plt.scatter(np.array(coord_infectes_1)[:,0],np.array(coord_infectes_1)[:,1],c='r')
-#plt.xlim(min(x[:,0])-1,max(x[:,0])+1)
-#plt.ylim(min(x[:,1])-1,max(x[:,1])+1)
-plt.title('2e vague de contamination')
-plt.show()
-
-plt.clf()
-plt.pie([len(coord_infectes_1),len(cas_sains_1)],labels=('infectés','sains'),shadow=True,autopct='%1.1f%%')
-plt.title('2er vague de contamination')
-plt.show()
-
-""" 3e vague """
-
-cas_sains_2 = [] # les individus sains sont de moins en moins
-coord_infectes_2 = coord_infectes_1
-
-for k in range (len(coord_infectes_1)):
-    for j in range (len(cas_sains_1)):
-        if distance(np.array(coord_infectes_2)[k,:],np.array(cas_sains_1)[j,:]) <0.5 and np.array(cas_sains_1)[j,:] not in np.array(coord_infectes_2) :
-            coord_infectes_2.append(np.array(cas_sains_1)[j,:])
-        elif np.array(cas_sains_1)[j,:] not in np.array(cas_sains_2) :
-            cas_sains_2.append(np.array(cas_sains_1)[j,:])
-
-plt.scatter(np.array(cas_sains_2)[:,0],np.array(cas_sains_2)[:,1],c='b')
-plt.scatter(np.array(coord_infectes_2)[:,0],np.array(coord_infectes_2)[:,1],c='r')
-plt.title('3e vague de contamination')
-plt.show()
-
-plt.clf()
-plt.pie([len(coord_infectes_2),len(cas_sains_2)],labels=('infectés','sains'),shadow=True,autopct='%1.1f%%')
-plt.title('3e vague de contamination')
-plt.show()
-
-""" résumé des 3 premières vagues """
 
 def evolution_virus(nb_individu,variance_population,rayon_contamination):
     # recommandé : nb_individu=150, rayon_contamination=1, variance_population=0.9
@@ -182,8 +89,5 @@ def evolution_virus(nb_individu,variance_population,rayon_contamination):
 
     plt.show()
 
-""" V) Avec n vagues """
-
-def n_vagues():
 
     
