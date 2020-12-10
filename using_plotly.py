1b17cd87f5df844044074916390f955d0b7407d5
Antonin <antoninlefevre45@icloud.com>
2020-12-10 20:35:36 +0100

    Update using_plotly.py

--- using_plotly.py
+++ using_plotly.py
@@ -241,14 +241,18 @@ def virus_px():
 
 # affiche la vague pour laquelle le virus ne se propage plus, avec les proportions et courbes évolutives
 
-def vague_seuil_px():
+## Version optimisée !
 
-    nb_individu = 1000 # recommandé : 1000
-    variance_population = 5
+def vague_seuil_px_opti():
+
+    start = time.time()
+
+    nb_individu = 1500  # recommandé : 2000
+    variance_pop = 4
     rayon_contamination = 2
-    infectiosite = 0.8 # proba d'être infecté
-    p = 0.35 # proba d'être immunisé
-    d = 0.2 # proba de décès
+    infectiosite = 0.17
+    p = 0.2
+    d = 0.05
 
     # NOTE : si les courbes restent constantes, augmentez le rayon de contamination
     # si le virus est trés mortel il n'y aura pas beaucoup de propagation
@@ -258,111 +262,137 @@ def vague_seuil_px():
     # Vert : '#00CC96'
     # Violet : '#AB63FA'
 
+    if nb_individu < 10 or rayon_contamination <= 0:
+        return 'error, nb_individu and var_population and rayon_contamination must be >=10 and > 0'
+    if infectiosite < 0 or infectiosite > 1:
+        return 'error, infectiosité must be in [0,1]'
+    if p < 0 or p > 1:
+        return 'error, p must be in [0,1]'
+    if d < 0 or p > 1:
+        return 'error, d must be in [0,1]'
+
     # création des figures
     fig = go.Figure()
-    fig = make_subplots(rows=2, cols=2,column_widths=[0.8, 0.2],row_heights=[0.5,0.5],subplot_titles=["population","",""],specs=[[{'type': 'xy'},{'type': 'domain'}],[{'type': 'xy', 'colspan' : 2},None]],horizontal_spacing = 0.05,vertical_spacing=0.05)
+    fig = make_subplots(rows=2, cols=2, column_widths=[0.8, 0.2], row_heights=[0.5, 0.5],
+                        subplot_titles=["population", "", ""],
+                        specs=[[{'type': 'xy'}, {'type': 'domain'}], [{'type': 'xy', 'colspan': 2}, None]],
+                        horizontal_spacing=0.05, vertical_spacing=0.05)
 
     # création des courbes finales
-    courbe_sains = []
-    courbe_infectes = []
-    courbe_immunises = []
-    courbe_deces = []
-    courbe_removed = [] # somme des immunisés + décès
+    courbes = dict(courbe_sains = [],courbe_infectes = [],courbe_immunises = [],courbe_deces = [],courbe_removed = [])
 
     # dataset
-    x, y = make_blobs(n_samples=nb_individu, centers=2, cluster_std=variance_population)  # création du dataset
-    taille_pop = len(x)
+    x, y = make_blobs(n_samples=nb_individu, centers=1, cluster_std=variance_pop)
+    df = pd.DataFrame(dict(x=x[:,0],
+                           y=x[:,1]))
+    taille_pop = len(df['x'])
 
-    numero_infecte_1 = rd.randint(0, taille_pop-1)  # on choisit le premier individu infecté au hasard
-    coord_1er_infecte = [x[:, 0][numero_infecte_1], x[:, 1][numero_infecte_1]]  # coordonnées du 1er infecté
-    courbe_sains.append(taille_pop - 1)
-    courbe_infectes.append(1)
-    courbe_immunises.append(0)
-    courbe_deces.append(0)
-    courbe_removed.append(0)
+    numero_infecte_1 = rd.randint(0, taille_pop - 1)  # on choisit le premier individu infecté au hasard
+    coord_1er_infecte = [df['x'][numero_infecte_1], df['y'][numero_infecte_1]]  # coordonnées du 1er infecté
+    courbes['courbe_sains'].append(taille_pop-1)
+    courbes['courbe_infectes'].append(1)
+    courbes['courbe_immunises'].append(0)
+    courbes['courbe_deces'].append(0)
+    courbes['courbe_removed'].append(0)
 
     # 1er vague
-    coord_infectes = []  # cette liste sera implémentée des nouveaux cas
-    coord_sains = []  # liste des cas sains
+
+    df_sans1erinfecte = df[(df['x'] != df['x'][numero_infecte_1]) & (df['y'] != df['y'][numero_infecte_1])]
+    coord = dict(coord_infectes=[],coord_sains=[],coord_immunises=[],coord_deces=[])
     for k in range(taille_pop):
-        if [x[:, 0][k], x[:, 1][k]] == coord_1er_infecte:
-            coord_infectes.append(coord_1er_infecte)
-        elif distance(coord_1er_infecte, [x[:, 0][k], x[:, 1][k]]) < rayon_contamination and chance_infecte(
-                infectiosite):
-            coord_infectes.append([x[:, 0][k], x[:, 1][k]])
+        if [df['x'][k], df['y'][k]] == coord_1er_infecte:
+            coord['coord_infectes'].append(coord_1er_infecte)
+        elif distance_e(coord_1er_infecte, [df_sans1erinfecte['x'][k], df_sans1erinfecte['y'][k]]) < rayon_contamination and chance_infecte(infectiosite):
+            coord['coord_infectes'].append([df['x'][k], df['y'][k]])
         else:
-            coord_sains.append([x[:, 0][k], x[:, 1][k]])
-    # actualisation des courbes finales
-    courbe_sains.append(len(coord_sains))
-    courbe_infectes.append(len(coord_infectes))
-    courbe_immunises.append(0)
-    courbe_deces.append(0)
-    courbe_removed.append(0)
+            coord['coord_sains'].append([df['x'][k], df['y'][k]])
 
-    # vagues 2 à la fin
-    coord_immunises = []  # on initialise
-    coord_deces = []
+    courbes['courbe_sains'].append(len(coord['coord_sains']))
+    courbes['courbe_infectes'].append(len(coord['coord_infectes']))
+    courbes['courbe_immunises'].append(0)
+    courbes['courbe_deces'].append(0)
+    courbes['courbe_removed'].append(0)
+
+    # vagues 2 à n
+
     i = 1
-    while len(coord_infectes)>0.1*taille_pop or len(courbe_sains)<5 : # conditions d'état stationnaire
+    while len(coord['coord_infectes']) > 0.05 * taille_pop or len(courbes['courbe_sains']) < 15:
         non_sains = []
-        coord_infectes1, coord_immunises = immuniser(coord_infectes, coord_immunises, p) # on sépare immunisés et infectés
-        coord_infectes, coord_deces = deces(coord_infectes1, coord_deces, coord_immunises, d) # on sépare les décès des infectés qu'ils restent
+        coord_infectes1, coord['coord_immunises'] = immuniser(coord['coord_infectes'], coord['coord_immunises'], p)
+        coord['coord_infectes'], coord['coord_deces'] = deces(coord_infectes1, coord['coord_deces'], coord['coord_immunises'], d)
 
-        for k in range(len(coord_infectes)):
-            for j in range(len(coord_sains)):
-                if distance(np.array(coord_infectes)[k, :],
-                            np.array(coord_sains)[j, :]) < rayon_contamination and np.array(coord_sains)[j,:] not in np.array(coord_infectes) and chance_infecte(infectiosite):
-                    coord_infectes.append(list(np.array(coord_sains)[j, :]))
-                    non_sains.append(list(np.array(coord_sains)[j, :]))
-        coord_sains = remove_(coord_sains, non_sains)
+        for k in range(len(coord['coord_infectes'])):
+            for j in range(len(coord['coord_sains'])):
+                if distance_e(coord['coord_infectes'][k],coord['coord_sains'][j]) < rayon_contamination and coord['coord_sains'][j] not in coord['coord_infectes'] and chance_infecte(infectiosite):
+                    coord['coord_infectes'].append(coord['coord_sains'][j])
+                    non_sains.append(coord['coord_sains'][j])
+        coord['coord_sains'] = remove_(coord['coord_sains'], non_sains)
         # pour les courbes finales
-        courbe_sains.append(len(coord_sains))
-        courbe_infectes.append(len(coord_infectes))
-        courbe_immunises.append(len(coord_immunises))
-        courbe_deces.append(len(coord_deces))
-        courbe_removed.append(len(coord_deces)+len(coord_immunises))
-        i += 1 # vague suivante
-        
-    # on trace les individus de la dernière vague si il y en a 
-    if coord_sains != []:
-        fig.add_trace(go.Scatter(x=np.array(coord_sains)[:, 0], y=np.array(coord_sains)[:, 1],name="sain", mode="markers", marker=dict(color='#636EFA'), showlegend=False),1,1)
-    if coord_infectes != []:
-        fig.add_trace(go.Scatter(x=np.array(coord_infectes)[:, 0], y=np.array(coord_infectes)[:, 1],name="infecté", mode="markers",marker=dict(color='#EF553B'), showlegend=False),1,1)
-    if coord_immunises != []:
-        fig.add_trace(go.Scatter(x=np.array(coord_immunises)[:, 0], y=np.array(coord_immunises)[:, 1],name='immunisé', mode="markers",marker=dict(color='#00CC96'), showlegend=False),1,1)
-    if coord_deces != []:
-        fig.add_trace(go.Scatter(x=np.array(coord_deces)[:, 0], y=np.array(coord_deces)[:, 1],name="décédé", mode="markers", marker=dict(color='#AB63FA'), showlegend=False),1,1)
-    
-    fig.update_traces(hoverinfo="name") # affichage du curseur de la souris
-    fig.update_xaxes(showgrid=False, visible=False, row=1, col=1) # on supprime les axes
-    fig.update_yaxes(showgrid=False, visible=False, row=1, col=1)
+        courbes['courbe_sains'].append(len(coord['coord_sains']))
+        courbes['courbe_infectes'].append(len(coord['coord_infectes']))
+        courbes['courbe_immunises'].append(len(coord['coord_immunises']))
+        courbes['courbe_deces'].append(len(coord['coord_deces']))
+        courbes['courbe_removed'].append(len(coord['coord_immunises']) + len(coord['coord_deces']))
+        i += 1  # vague suivante
+    if coord['coord_sains'] != []:
+        fig.add_trace(go.Scatter(x=np.array(coord['coord_sains'])[:, 0], y=np.array(coord['coord_sains'])[:, 1], name="sain", mode="markers",
+                                 marker=dict(
+                                     line=dict(
 
-    # Pie finale
-    labels = ["sains","infectés","immunisés","décédés"]
-    fig.add_trace(go.Pie(values=[len(coord_sains),len(coord_infectes) ,len(coord_immunises), len(coord_deces)], labels=labels, sort=False),1,2)
-    
-    # on trace les courbes finales
-    x_courbe = list(np.arange(0, len(courbe_sains))) # abscisses, correspond aux vagues
-    fig.add_trace(go.Scatter(x=x_courbe, y=courbe_sains, marker=dict(color='#636EFA'), showlegend=False, name="sains", yaxis="y",),2,1)
-    fig.add_trace(go.Scatter(x=x_courbe, y=courbe_infectes,  marker=dict(color='#EF553B'), showlegend=False, name="infectés", yaxis="y2",),2,1)
-    fig.add_trace(go.Scatter(x=x_courbe, y=courbe_immunises, marker=dict(color='#00CC96'), showlegend=False, name="immunisés", yaxis="y3",),2,1)
-    fig.add_trace(go.Scatter(x=x_courbe, y=courbe_deces, marker=dict(color='#AB63FA'), showlegend=False, name="décédés", yaxis="y4",),2,1)
-    fig.add_trace(go.Scatter(x=x_courbe, y=courbe_removed, marker=dict(color='#000000'), showlegend=False, name="removed", yaxis="y5",),2,1)
-    fig.update_xaxes(title_text="vague", row=2, col=1) # nom abscisses
-    fig.update_yaxes(title_text="nombre d'individus", row=2, col=1) # nom ordonnées
-    fig.add_annotation(text="Maximum d'infectés", x=courbe_infectes.index(max(courbe_infectes)), # ajouter un texte avec une flèche au pic d'infectés
-                   y=max(courbe_infectes)+0.03*taille_pop, arrowhead=1, showarrow=True,row=2,col=1)
+                                         color='#636EFA')
+                                 ), showlegend=False), 1, 1)
+    if coord['coord_infectes'] != []:
+        fig.add_trace(go.Scatter(x=np.array(coord['coord_infectes'])[:, 0], y=np.array(coord['coord_infectes'])[:, 1], name="infecté",mode="markers",
+                                 marker=dict(
+                                     line=dict(
+
+                                         color='#EF553B')
+                                 ), showlegend=False), 1, 1)
+    if coord['coord_immunises'] != []:
+        fig.add_trace(go.Scatter(x=np.array(coord['coord_immunises'])[:, 0], y=np.array(coord['coord_immunises'])[:, 1], name='immunisé',mode="markers",
+                                 marker=dict(
+                                     line=dict(
+
+                                         color='#00CC96')
+                                 ), showlegend=False), 1, 1)
+    if coord['coord_deces'] != []:
+        fig.add_trace(go.Scatter(x=np.array(coord['coord_deces'])[:, 0], y=np.array(coord['coord_deces'])[:, 1], name="décédé", mode="markers",
+                                 marker=dict(
+                                     line=dict(
+
+                                         color='#AB63FA')
+                                 ), showlegend=False), 1, 1)
+
+    fig.update_traces(hoverinfo="name")
+    fig.update_xaxes(showgrid=False, visible=False, row=1, col=1)
+    fig.update_yaxes(showgrid=False, visible=False, row=1, col=1)
+    titre = str(i) + '-ième vague'
+    labels = ["sains", "infectés", "immunisés", "décédés"]
+    fig.add_trace(go.Pie(values=[len(coord['coord_sains']), len(coord['coord_infectes']), len(coord['coord_immunises']), len(coord['coord_deces'])], labels=labels, sort=False), 1, 2)
+
+    x_courbe = list(np.arange(0, len(courbes['courbe_sains'])))
+    fig.add_trace(go.Scatter(x=x_courbe, y=courbes['courbe_sains'], marker=dict(color='#636EFA'), showlegend=False, name="sains",yaxis="y", ), 2, 1)
+    fig.add_trace(go.Scatter(x=x_courbe, y=courbes['courbe_infectes'], marker=dict(color='#EF553B'), showlegend=False, name="infectés",yaxis="y2", ), 2, 1)
+    fig.add_trace(go.Scatter(x=x_courbe, y=courbes['courbe_immunises'], marker=dict(color='#00CC96'), showlegend=False, name="immunisés",yaxis="y3", ), 2, 1)
+    fig.add_trace(go.Scatter(x=x_courbe, y=courbes['courbe_deces'], marker=dict(color='#AB63FA'), showlegend=False, name="décédés",yaxis="y4", ), 2, 1)
+    fig.add_trace(go.Scatter(x=x_courbe, y=courbes['courbe_removed'], marker=dict(color='#000000'), showlegend=False, name="removed",yaxis="y5", ), 2, 1)
+    fig.update_xaxes(title_text="jours", row=2, col=1)
+    fig.update_yaxes(title_text="nombre d'individus", row=2, col=1)
+    fig.add_annotation(text="Maximum d'infectés", x=courbes['courbe_infectes'].index(max(courbes['courbe_infectes'])),# ajouter un texte avec une flèche
+                       y=max(courbes['courbe_infectes']) + 0.03 * taille_pop, arrowhead=1, showarrow=True, row=2, col=1)
     fig.update_traces(
-        hoverinfo="name+x+y", # affichage du curseur de la souris sur le point
+        hoverinfo="name+x+y",
         line={"width": 1.2},
         marker={"size": 6},
         mode="lines+markers",
         showlegend=False, row=2, col=1)
     fig.update_layout(hovermode="x")
-    fig.update_layout(title_text="simulation virus") # titre du plot
-    fig.update_layout(title_font_color='#EF553B',) # couleur du titre
-    
+    fig.update_layout(title_text="simulation virus")
+    fig.update_layout(title_font_color='#EF553B')
+    print('temps d\'éxecution:')
+    print(time.time()-start)
     plot(fig)
+
 
 
 
